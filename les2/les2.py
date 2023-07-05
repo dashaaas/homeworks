ea55de391a9ac928bdfadd117d55ff845cf19dc5
dasha <darasajkina@gmail.com>
2023-07-05 13:00:08 +0300

    ver1 add les2

---
+++ les2/les2.py
@@ -0,0 +1,100 @@
+1 задание:
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name ="viewport"
+          content='width=device-width, user-scalable=no, inital-scale=1.8,maximum-scale=1.0, minimum-scale=1.0'>
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Доска объявленмй</title>
+    <link rel="stylesheet" href="file.css">
+</head>
+<body>
+<h1>Заголовок 1</h1>
+<h2>Заголовок 2</h2>
+<h3>Заголовок 3</h3>
+<h4>Заголовок 4</h4>
+<h5>Заголовок 5</h5>
+<h6>Заголовок 6</h6>
+<p class="parag1">
+    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque minima quaerat saepe! Expedita necessitatibus
+    quod recusandae sed sint! Accusamus, consequatur distinctio dolor ipsam laboriosam modi nam numquam perferendis
+    placeat quidem!
+</p>
+<p class="parag2">
+    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias aperiam deleniti, eaque ex nisi praesentium sed.
+    Consequuntur et excepturi id omnis optio quia vitae voluptas! Doloremque ipsum quod velit vero!
+</p>
+<p class="parag3">
+    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad aspernatur consectetur cum, deleniti dignissimos
+    dolore ea ex excepturi explicabo impedit iste maiores molestias necessitatibus nemo nisi, nostrum nulla porro
+    temporibus
+</p>
+<p class="parag4">
+    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab commodi culpa cupiditate dolor, eum fugit ipsum
+    laboriosam natus nobis perferendis quidem saepe sequi, voluptates! Delectus ea molestiae nihil quas
+    similique!
+</p>
+<span>
+   Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quisquam tenetur unde voluptas! Asperiores dolore earum,
+    eveniet maxime natus repellat sed sunt temporibus unde voluptatibus. Consequuntur debitis est ipsa officiis tempore?
+</span>
+<span>
+    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias architecto at consectetur consequuntur deleniti
+    eaque earum fugit hic iste, itaque laboriosam nisi nulla numquam officiis omnis qui reiciendis rerum soluta.
+</span>
+
+<div>
+    <span>
+   Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quisquam tenetur unde voluptas! Asperiores dolore earum,
+    eveniet maxime natus repellat sed sunt temporibus unde voluptatibus. Consequuntur debitis est ipsa officiis tempore?
+    </span>
+</div>
+<div>
+    <span>
+    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias architecto at consectetur consequuntur deleniti
+    eaque earum fugit hic iste, itaque laboriosam nisi nulla numquam officiis omnis qui reiciendis rerum soluta.
+    </span>
+</div>
+
+<ul>
+    <li>Первый элемент</li>
+    <li>Второй элемент</li>
+    <li>Третий элемент</li>
+</ul>
+<img src="file.PNG">
+</body>
+</html>
+
+2 задание:
+.parag1 {
+    background: blue;
+    font-size: 32pt;
+    color: green;
+}
+
+.parag2 {
+    border: 10px solid yellow;
+    border-radius: 125px;
+    font-size: 24pt;
+    padding-left: 200px;
+    padding-bottom: 200px;
+    margin-top: 200px;
+}
+
+.parag3 {
+    font-weight: bold;
+    font-size: 22px;
+    font-family: Courier New;
+}
+
+div {
+    background: red;
+}
+
+.parag4{
+    font-style: italic;
+    text-align: center;
+    font-size: 24px;
+    text-shadow: green 5px 10px;
+}
