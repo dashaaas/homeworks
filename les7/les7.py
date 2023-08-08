580ff53fdb235b88ceccfd48e1f363e1296ea0c3
dasha <darasajkina@gmail.com>
2023-08-08 16:50:10 +0300

    ver1 add les7

---
+++ les7/les7.py
@@ -0,0 +1,17 @@
+models.py:
+from django.db import models
+
+class Advertisement(models.Model):
+    title = models.CharField('Заголовок', max_length=128, )
+    description = models.TextField('Описание')
+    price = models.DecimalField('Цена', max_digits=10, decimal_places=2)
+    auction = models.BooleanField('Торг', help_text='Отметьте, уместен ли торг')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = 'advertisements'
+
+    def __str__(self):
+        return f'Advertisement(id={self.id}, title={self.title}, price={self.price})'
+
